7b9d6447d8075f337270dec6fe05c22a3bd0ca87
Zhentao-Lin <62634190+Zhentao-Lin@users.noreply.github.com>
2021-09-01 16:55:40 +0800

    Update 08.1_Serial_Print_by_Usart1.py

--- Python/Python_Codes/08.1_Serial_Print/08.1_Serial_Print_by_Usart1.py
+++ Python/Python_Codes/08.1_Serial_Print/08.1_Serial_Print_by_Usart1.py
@@ -1,9 +1,9 @@
 from machine import UART, Pin
 import time
 
-#tx = Pin(4) or Pin(7)
-#rx = Pin(5) or Pin(8)
-myUsart1 = UART(1, baudrate=115200, bits=8, tx=Pin(4), rx=Pin(5), timeout=10) 
+#tx = Pin(4) or Pin(8)
+#rx = Pin(5) or Pin(9)
+myUsart1 = UART(1, baudrate=115200, bits=8, tx=Pin(8), rx=Pin(9), timeout=10) 
 myUsart1.write("Raspberry Pi Pico initialization completed!\r\n")
 
 while True:
